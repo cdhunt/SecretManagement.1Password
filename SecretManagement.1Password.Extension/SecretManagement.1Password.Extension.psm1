b8285a1530004f7751b4e7b21ec1e25dc4249b14
Chris Hunt <chris.hunt@hey.com>
2020-11-18 09:52:08 -0500

    Add support for more Secret types

--- SecretManagement.1Password.Extension/SecretManagement.1Password.Extension.psm1
+++ SecretManagement.1Password.Extension/SecretManagement.1Password.Extension.psm1
@@ -61,21 +61,29 @@ function Get-SecretInfo {
     )
 
     # Password category isn't supported for some reason
-    $items = & op list items --categories Login --vault $VaultName | ConvertFrom-Json
+    $items = & op list items --categories Login, Password --vault $VaultName | ConvertFrom-Json
 
+    $keyList = [Collections.ArrayList]::new()
 
     foreach ($item in $items) {
-        $type = switch ($item.templateUuid) {
-            '001' { [SecretType]::PSCredential }
-            '005' { [SecretType]::SecureString }
-            Default { [SecretType]::Unknown }
+        if ( $keyList.Contains($item.overview.title) ) {
+            Write-Verbose "Get-SecretInfo: An item with the same key has already been added. Key: [$($item.overview.title)]"
         }
+        else {
+            $type = switch ($item.templateUuid) {
+                '001' { [SecretType]::PSCredential }
+                '005' { [SecretType]::SecureString }
+                Default { [SecretType]::Unknown }
+            }
 
-        [SecretInformation]::new(
-            $item.overview.title,
-            $type,
-            $VaultName
-        )
+            Write-Verbose $item.overview.title
+            [SecretInformation]::new(
+                $item.overview.title,
+                $type,
+                $VaultName
+            )
+            $keyList.Add($item.overview.title)
+        }
     }
 }
 
@@ -92,7 +100,7 @@ function Get-Secret {
         [hashtable] $AdditionalParameters
     )
 
-    $item = & op get item $Name --fields username,password --vault $VaultName 2>$null | ConvertFrom-Json
+    $item = & op get item $Name --fields username,password --vault $VaultName 2>$null | ConvertFrom-Json -AsHashtable
 
     [securestring]$secureStringPassword = ConvertTo-SecureString $item.password -AsPlainText -Force
 
@@ -121,48 +129,119 @@ function Set-Secret {
     )
 
     $item = & op get item $Name --fields title --vault $VaultName 2>$null
+    $verb = if ($null -eq $item) { 'create' } else { 'edit' }
+    Write-Verbose $verb
+    $data = @{}
+    $commandArgs = [Collections.ArrayList]::new()
 
-    $date = @{}
-
+    Write-Verbose "Secret type [$($Secret.GetType().Name)]"
     switch ($Secret.GetType().Name) {
-        'string' { Write-Warning "String input is not yet implemented" }
-        'securestring' { Write-Warning "SecureString input is not yet implemented" }
-        'PSCredential' {
-            $data =  op get template login | ConvertFrom-Json -AsHashtable
+        { 'string' -or 'Int32' -or 'Int64' -or 'Double' } {
+            $category = "Password"
+            Write-Verbose "Processing [string] as $category"
+            $commandArgs.Add($verb) | Out-Null
+            $commandArgs.Add('item') | Out-Null
 
-            $data.fields | ForEach-Object {
-                if ($_.name -eq 'username') {$_.value = $Secret.UserName}
-                if ($_.name -eq 'password') {$_.value = $Secret.GetNetworkCredential().password}
+            if ('create' -eq $verb ) {
+                Write-Verbose "Creating $Name"
+                $data = op get template $category | ConvertFrom-Json -AsHashtable
+                $data.password = $Secret
+                $endcodedData = $data | ConvertTo-Json | op encode
+
+                $commandArgs.Add($category) | Out-Null
+                $commandArgs.Add($endcodedData) | Out-Null
+                $commandArgs.Add("Title=$Name") | Out-Null
             }
+            else {
+                Write-Verbose "Updating $item"
+                $commandArgs.Add($item) | Out-Null
+                $commandArgs.Add("password=$Secret") | Out-Null
+            }
+            break
+        }
+        'securestring' {
+            $category = "Password"
+            Write-Verbose "Processing [securestring] as $category"
+            $commandArgs.Add($verb) | Out-Null
+            $commandArgs.Add('item') | Out-Null
 
-            $endcodedData = $data | ConvertTo-Json | op encode
+            if ('create' -eq $verb ) {
+                Write-Verbose "Creating $Name"
+                $data = op get template $category | ConvertFrom-Json -AsHashtable
+                $data.password = ConvertFrom-SecureString -SecureString $Secret -AsPlainText
+                $endcodedData = $data | ConvertTo-Json | op encode
 
+                $commandArgs.Add($category) | Out-Null
+                $commandArgs.Add($endcodedData) | Out-Null
+                $commandArgs.Add("Title=$Name") | Out-Null
+            }
+            else {
+                Write-Verbose "Updating $item"
+                $commandArgs.Add($item) | Out-Null
+                $commandArgs.Add("password=$(ConvertFrom-SecureString -SecureString $Secret -AsPlainText)") | Out-Null
+            }
+            break
+        }
+        'PSCredential' {
             $category = "Login"
-         }
+            Write-Verbose "Processing [PSCredential] as $category"
+            $commandArgs.Add($verb) | Out-Null
+            $commandArgs.Add('item') | Out-Null
+
+            if ('create' -eq $verb ) {
+                Write-Verbose "Creating $Name"
+                $data = op get template $category | ConvertFrom-Json -AsHashtable
+                $data.fields | ForEach-Object {
+                    if ($_.name -eq 'username') { $_.value = $Secret.UserName }
+                    if ($_.name -eq 'password') { $_.value = ConvertFrom-SecureString -SecureString $Secret -AsPlainText }
+                }
+                $endcodedData = $data | ConvertTo-Json | op encode
+
+                $commandArgs.Add($category) | Out-Null
+                $commandArgs.Add($endcodedData) | Out-Null
+                $commandArgs.Add("Title=$Name") | Out-Null
+            }
+            else {
+                Write-Verbose "Updating $item"
+                $commandArgs.Add($item) | Out-Null
+                $commandArgs.Add("username=$($Secret.UserName)") | Out-Null
+                $commandArgs.Add("password=$(ConvertFrom-SecureString -SecureString $Secret -AsPlainText)") | Out-Null
+            }
+            break
+        }
         Default {}
     }
 
-    if ($null -eq $item) {
-        & op create item $category $endcodedData title=$Name --vault $VaultName
-    } else {
-        & op edit item $item username=$($Secret.UserName) password=$($Secret.GetNetworkCredential().password) --vault $VaultName
+    $commandArgs.Add('--vault') | Out-Null
+    $commandArgs.Add($VaultName) | Out-Null
+
+    $sanitizedArgs = $commandArgs | ForEach-Object {
+        if ($_ -like 'password=*') {
+            'password=*****'
+        }
+        else {
+
+            $_
+        }
+    }
+    Write-Verbose ($sanitizedArgs -join ' ')
+        & op @commandArgs
+
+        return $?
     }
 
-    return $?
-}
-
-function Remove-Secret {
-    [CmdletBinding()]
-    param (
-        [Parameter()]
-        [string]$Name,
-        [Parameter()]
-        [string]$VaultName,
-        [Parameter()]
-        [hashtable] $AdditionalParameters
-    )
+    function Remove-Secret {
+        [CmdletBinding()]
+        param (
+            [Parameter()]
+            [string]$Name,
+            [Parameter()]
+            [string]$VaultName,
+            [Parameter()]
+            [hashtable] $AdditionalParameters
+        )
 
 
-    Write-Warning "Not implemented"
-    return $false
-}
+        Write-Warning "Not implemented"
+        return $false
+    }
